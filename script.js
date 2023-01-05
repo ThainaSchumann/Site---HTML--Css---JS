093ab1125ba5e6b7d6116771f471bdae1dd3e704
Thaina <thschumann85@outlook.de>
2023-01-05 21:41:02 +0100

    :package: budget calculation

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle("show-menu")
-);
+    document.querySelector(".container").classList.toggle("show-menu"));
 
-document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#quantity").addEventListener("change", atualizarPreco)
 document.querySelector("#js").addEventListener("change", atualizarPreco)
-document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
-document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
-document.querySelector("#prazo").addEventListener("change", function () {
-    const prazo = document.querySelector("#prazo").value
-    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+document.querySelector("#layout-yes").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-no").addEventListener("change", atualizarPreco)
+document.querySelector("#period").addEventListener("change", function () {
+    const period = document.querySelector("#period").value
+    document.querySelector("label[for=period]").innerHTML = `period: ${period} weeks`
     atualizarPreco()
-})
+});
 
 function atualizarPreco(){
-    const qtde = document.querySelector("#qtde").value
-    const temJS = document.querySelector("#js").checked
-    const incluiLayout = document.querySelector("#layout-sim").checked
-    const prazo = document.querySelector("#prazo").value
+    const quantity = document.querySelector("#quantity").value
+    const hasJS = document.querySelector("#js").checked
+    const includeLayout = document.querySelector("#layout-yes").checked
+    const period = document.querySelector("#period").value
   
-    let preco = qtde * 100;
-    if (temJS) preco *= 1.1
-    if (incluiLayout) preco += 500
-    let taxaUrgencia = 1 - prazo*0.1;
-    preco *= 1 + taxaUrgencia
+    let preis = quantity * 100;
+    if (hasJS) preis *= 1.1
+    if (includeLayout) preis += 500
+    let taxaUrgencia = 1 - period*0.1;
+    preis *= 1 + taxaUrgencia
    
-    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+    document.querySelector("#preis").innerHTML = `$ ${preis.toFixed(2)}`
 }
